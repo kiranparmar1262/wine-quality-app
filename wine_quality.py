b8567e4dd09e45e026f440bb052da1a42762ad6e
kiranparmar1262 <80308330+kiranparmar1262@users.noreply.github.com>
2021-03-13 16:34:45 +0530

    Add files via upload

---
+++ wine_quality.py
@@ -0,0 +1,66 @@
+
+import streamlit as st
+import pickle
+
+pickle_in = open('model.pkl', 'rb') 
+classifier = pickle.load(pickle_in)
+
+@st.cache
+def prediction(residualsugar, freesulfurdioxide, chlorides, volatileacidity,alcohol):   
+ 
+    # Pre-processing user input      
+    
+    # Making predictions 
+    prediction = classifier.predict([[residualsugar, freesulfurdioxide, chlorides, volatileacidity,alcohol]])
+    
+    
+    if prediction == 0:
+        pred = 'Not Good'
+    else:
+        pred = "Good"
+    return pred
+  
+# this is the main function in which we define our webpage  
+def main():       
+    # front end elements of the web page 
+    html_temp = """ 
+    <div style ="background-color:black;border-radius: 10px;
+  padding: 14px">
+    <h1 style ="color:white;text-align:center;">Wine Quality Prediction App</h1> 
+    </div> 
+    """
+    
+    page_bg_img = '''
+
+    <style>
+    body {
+    
+    background-image: url("https://image.freepik.com/free-photo/abstract-black-white-bokeh-background_1962-1324.jpg");
+    background-size: cover;
+    }
+    </style>
+    '''
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    
+   
+    #st.image('houseprice.jpg')  
+    # display the front end aspect
+    st.markdown(html_temp, unsafe_allow_html = True)   
+    # following lines create boxes in which user can enter data required to make prediction 
+    
+#     Gender = st.selectbox('Gender',("Male","Female"))      this is for drop down box
+#     Married = st.selectbox('Marital Status',("Unmarried","Married")) 
+    residualsugar = st.number_input("Residualsugar")
+    freesulfurdioxide = st.number_input("Freesulfurdioxide")
+    chlorides = st.number_input("Chorides")
+    volatileacidity = st.number_input("Volatileacidity")
+    alcohol = st.number_input("Alcohol")
+    result = ""
+      
+    # when 'Predict' is clicked, make the prediction and store it 
+    if st.button("Predict"):
+        result = prediction(residualsugar, freesulfurdioxide, chlorides, volatileacidity,alcohol)
+        st.success('Winew Quality is {}'.format(result))
+     
+if __name__=='__main__': 
+    main()
